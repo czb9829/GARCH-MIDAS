75a66197ea40a298c1755b741bbbc2baf28070c2
Fengqi Liu <123794116+FrankLeo666@users.noreply.github.com>
2023-01-28 14:37:53 +0800

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,6 +1,6 @@
 """
 garch-midas model
-参数估计(全样本)
+coefficients estimation
 """
 import numpy as np
 import pandas as pd
@@ -8,120 +8,122 @@ from mfgarch import fit_mfgarch, plot_mfgarch
 
 pd.set_option("display.max_columns", None)
 
-# 读取数据
+# read data
 df = pd.read_csv("../data/data_mfgarch.csv")
 
 
 
 # *************************************************************************************
-#                             单因子garch-midas模型估计
+#                             single-variable garch-midas model
 # *************************************************************************************
 
-# 估计模型
-result = fit_mfgarch(data=df,  # 数据框
-                     y='return',  # 高频变量
-                     x='nfci',  # 低频变量
-                     low_freq='year_week',  # 低频变量频率
-                     K=52,  # 低频变量滞后阶数
-                     gamma=True,  # GJR-GARCH形式: gamma = True; GARCH(1, 1)形式： gamma = False
-                     w_restricted=True)  # 权重函数参数w1 = 1: w_restricted = True; w1为待估计参数：w_restricted = False
-print(result.estimate)  # 参数估计结果
-print(result.fitted.head())  # 长短期波动拟合值
-print(result.llh, result.aic, result.bic)  # 对数似然与信息准则
-print(result.variance_ratio)  # 低频变量的解释程度
-print(result.est_weight)  # 低频变量滞后期的权重
-print(result.mf_forecast)  # 波动率预测值, 预测期数由参数n_horizons设定
-plot_mfgarch(result)  # 画图: 方差形式的波动率
-plot_mfgarch(result, is_sqrt=True)  # 画图: 标准差形式的波动率
+# coefficients estimation
+result = fit_mfgarch(data=df,  # select the in-sample data
+                     y='return',  # high-frequency variable
+                     x='nfci',  # low-frequency variable
+                     low_freq='year_week',  
+                     K=52,  # the lag-order of low-frequency variable
+                     gamma=True,  # GJR-GARCH for short-term volatility: gamma = True; GARCH(1, 1)： gamma = False
+                     w_restricted=True)  # parameters for the weighting scheme. 
+                                         #If set w1 = 1: w_restricted = True; if w1 not restricted：w_restricted = False
+
+print(result.estimate)  # coefficients estimation results
+print(result.fitted.head())  # fitting value of short-term and long-term volatility
+print(result.llh, result.aic, result.bic)  # log-likeihood function, AIC and BIC
+print(result.variance_ratio)  # print the variance_ratio, which measures the contribution of the long-term volatility to the total volatility
+print(result.est_weight)  # estimated parameters for the weighting scheme
+plot_mfgarch(result)  # visulization of volatility (in the form of variance)
+plot_mfgarch(result, is_sqrt=True)  # visulization of volatility (in the form of standard deviation)
 
 # *************************************************************************************
-#                             双因子garch-midas模型估计
+#                             two-variable garch-midas model
 # *************************************************************************************
-result = fit_mfgarch(data=df,  # 数据框
-                     y='return',  # 高频变量
-                     x=['dindpro', 'nfci'],  # 双低频变量
-                     low_freq=['year_month', 'year_week'],  # 低频变量频率
-                     K=[12, 52],  # 低频变量滞后阶数
-                     gamma=True,  # GJR-GARCH形式: gamma = True; GARCH(1, 1)形式： gamma = False
+result = fit_mfgarch(data=df,
+                     y='return',
+                     x=['dindpro', 'nfci'],
+                     low_freq=['year_month', 'year_week'],
+                     K=[12, 52],
+                     gamma=True,
                      theta_restricted=[False, True],
-                     w_restricted=[True, True])  # 权重函数参数w1 = 1: w_restricted = True; w1为待估计参数：w_restricted = False
-print(result.estimate)  # 参数估计结果
-# 画图
+                     w_restricted=[True, True]) 
+print(result.estimate)
+
+# visulization
 plot_mfgarch(result, is_sqrt=True)
 
 # *************************************************************************************
-#                            多因子garch-midas模型估计
+#                            multi-variable garch-midas model
 # *************************************************************************************
 
-result = fit_mfgarch(data=df,  # 数据框
-                     y='return',  # 高频变量
-                     x=['dindpro', 'nai', 'nfci'],  # 多低频变量
-                     low_freq=['year_month', 'year_month', 'year_week'],  # 低频变量频率
-                     K=[12, 12, 52],  # 低频变量滞后阶数
-                     gamma=True,  # GJR-GARCH形式: gamma = True; GARCH(1, 1)形式： gamma = False
+result = fit_mfgarch(data=df,
+                     y='return',
+                     x=['dindpro', 'nai', 'nfci'],
+                     low_freq=['year_month', 'year_month', 'year_week'],
+                     K=[12, 12, 52], 
+                     gamma=True, 
                      w_restricted=[True,
                                    True,
-                                   True])  # 权重函数参数w1 = 1: w_restricted = True; w1为待估计参数：w_restricted = False
-print(result.estimate)  # 参数估计结果
-# 画图
+                                   True])  
+print(result.estimate)
+
 plot_mfgarch(result, is_sqrt=True)
 
 # *************************************************************************************
-#                        包含虚拟变量的garch-midas模型估计1
+#                        garch-midas model with dummy variable I
 # *************************************************************************************
 year = pd.to_datetime(df["date"]).dt.year
 year = np.array(year)
 dummy = np.zeros(year.size)
 dummy[year > 2000] = 1
 df["dummy"] = dummy
-# 估计模型
-result = fit_mfgarch(data=df,  # 数据框
-                     y='return',  # 高频变量
-                     x='nfci',  # 低频变量
-                     low_freq='year_week',  # 低频变量频率
-                     dummy="dummy",  # 截距项中添加虚拟变量dummy
-                     K=52,  # 低频变量滞后阶数
-                     gamma=True,  # GJR-GARCH形式: gamma = True; GARCH(1, 1)形式： gamma = False
-                     w_restricted=True)  # 权重函数参数w1 = 1: w_restricted = True; w1为待估计参数：w_restricted = False
-print(result.estimate)  # 参数估计结果
+
+# coefficients estimation 
+result = fit_mfgarch(data=df,
+                     y='return',
+                     x='nfci',
+                     low_freq='year_week',
+                     dummy="dummy",  # including dummy variable into the intercept
+                     K=52,
+                     gamma=True,  
+                     w_restricted=True)
+print(result.estimate)
 
 # *************************************************************************************
-#                        包含虚拟变量的garch-midas模型估计2
+#                        garch-midas model with dummy variable II
 # *************************************************************************************
 year = pd.to_datetime(df["date"]).dt.year
 year = np.array(year)
 dummy = np.zeros(year.size)
 dummy[year > 2000] = 1
 df["dummy"] = dummy
-# 估计模型
-result = fit_mfgarch(data=df,  # 数据框
-                     y='return',  # 高频变量
-                     x='nfci',  # 低频变量
-                     low_freq='year_week',  # 低频变量频率
-                     dummy="dummy",  # 截距项中添加虚拟变量dummy
-                     interact_term={"x": "nfci", "dummy": "dummy"},  # 因子nfci的斜率中添加虚拟变量dummy
-                     K=52,  # 低频变量滞后阶数
-                     gamma=True,  # GJR-GARCH形式: gamma = True; GARCH(1, 1)形式： gamma = False
-                     w_restricted=True)  # 权重函数参数w1 = 1: w_restricted = True; w1为待估计参数：w_restricted = False
-print(result.estimate)  # 参数估计结果
+
+result = fit_mfgarch(data=df,
+                     y='return',
+                     x='nfci',
+                     low_freq='year_week',
+                     dummy="dummy", 
+                     interact_term={"x": "nfci", "dummy": "dummy"},  # including a interact term of the dummy variable and a long-term predictor
+                     K=52,
+                     gamma=True,
+                     w_restricted=True)
+print(result.estimate) 
 
 # *************************************************************************************
-#                        包含虚拟变量的garch-midas模型估计3
+#                        garch-midas model with dummy variable III
 # *************************************************************************************
 year = pd.to_datetime(df["date"]).dt.year
 year = np.array(year)
 dummy = np.zeros(year.size)
 dummy[year > 2000] = 1
 df["dummy"] = dummy
-# 估计模型
-result = fit_mfgarch(data=df,  # 数据框
-                     y='return',  # 高频变量
-                     x=['dindpro', 'nfci'],  # 双低频变量
-                     low_freq=['year_month', 'year_week'],  # 低频变量频率
-                     dummy="dummy",  # 截距项中添加虚拟变量dummy
+
+result = fit_mfgarch(data=df, 
+                     y='return', 
+                     x=['dindpro', 'nfci'],
+                     low_freq=['year_month', 'year_week'], 
+                     dummy="dummy",
                      interact_term={"x": ["dindpro", "nfci"], "dummy": ["dummy", "dummy"]},
-                     # 因子dindpro的斜率中添加虚拟变量dummy, 因子nfci的斜率中添加虚拟变量dummy
-                     K=[12, 52],  # 低频变量滞后阶数
-                     gamma=True,  # GJR-GARCH形式: gamma = True; GARCH(1, 1)形式： gamma = False
-                     w_restricted=[True, True])  # 权重函数参数w1 = 1: w_restricted = True; w1为待估计参数：w_restricted = False
-print(result.estimate)  # 参数估计结果
+                     K=[12, 52],
+                     gamma=True,  
+                     w_restricted=[True, True]) 
+print(result.estimate)
