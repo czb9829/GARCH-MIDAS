e81e2255e6f0582624cd40da2901da094a81d60b
Fengqi Liu <123794116+FrankLeo666@users.noreply.github.com>
2023-01-28 14:42:06 +0800

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -1,6 +1,6 @@
 """
 garch-midas model
-基于信息准则的最优滞后阶数选择
+selection of best lag-order based on AIC and BIC
 """
 
 import numpy as np
@@ -9,22 +9,22 @@ from mfgarch import optimal_lag_order
 
 pd.set_option("display.max_columns", None)
 
-# 读取数据
+# read data
 df = pd.read_csv("../data/data_mfgarch.csv")
 
-result = optimal_lag_order(data=df,  # 数据框
-                           y='return',  # 高频变量
-                           x=["dindpro", "nfci"],  # 低频变量
-                           low_freq=["year_month", "year_week"],  # 低频变量频率
+result = optimal_lag_order(data=df, 
+                           y='return',  # high-frequency variable
+                           x=["dindpro", "nfci"],  # low-frequency variable
+                           low_freq=["year_month", "year_week"],
                            var_lags={
-                               "dindpro": list(range(2, 37)),  # 因子dindpro的滞后阶数有35个可选值2~36
-                               "nfci": [52 for _ in range(35)],  # 因子nfci的滞后阶数固定为52
-                           },  # 低频变量滞后阶数
-                           gamma=True,  # GJR-GARCH形式: gamma = True; GARCH(1, 1)形式： gamma = False
+                               "dindpro": list(range(2, 37)),  # the lag-order of "dindpro" could be set from 2 to 36
+                               "nfci": [52 for _ in range(35)],  # the lag-order of "nfci" is fixed as 52
+                           },  
+                           gamma=True,  
                            w_restricted=[True,
-                                         True])  # 权重函数参数w1 = 1: w_restricted = True; w1为待估计参数：w_restricted = False
+                                         True]) 
 
 
-print(result.AIC_opt) # aic = 29125.810632781555, 最优滞后阶数： lag={'dindpro': 13, 'nfci': 52}
-print(result.BIC_opt) # aic = 29191.709178375833, 最优滞后阶数： lag={'dindpro': 13, 'nfci': 52}
+print(result.AIC_opt) # aic = 29125.810632781555, best lag-order： lag={'dindpro': 13, 'nfci': 52}
+print(result.BIC_opt) # aic = 29191.709178375833, best lag-order： lag={'dindpro': 13, 'nfci': 52}
 
