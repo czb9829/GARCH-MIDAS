a2d9f5e47588fff71b16b1ec65b690f20d9674b3
Fengqi Liu <123794116+FrankLeo666@users.noreply.github.com>
2023-01-28 15:18:29 +0800

    Update MCS Test.py

--- MCS Test.py
+++ MCS Test.py
@@ -6,7 +6,7 @@ import pandas as pd
 pd.set_option("display.max_columns", None)
 
 # read data
-df = pd.read_csv(r"--------RMSE.csv") # the csv file contains the values of loss functions
+df = pd.read_csv(r"--------LossFunc.csv") # the csv file contains the values of loss functions
 df = df.copy()[:60]
 
 mcs = MCS(df, size=0.1) # threshold of p-value
